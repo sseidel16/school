88aef1920a670dcae4cc467d4d02da8587e50833
Stephen Seidel <stephen.a.seidel@gmail.com>
2021-10-31 22:21:43 -0700

    prep for non-moodle resources by adding moodle prefix

--- config.docker-template.php
+++ config.docker-template.php
@@ -6,7 +6,7 @@ $CFG = new stdClass();
 
 $CFG->dbtype    = getenv('MOODLE_DOCKER_DBTYPE');
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = 'db';
+$CFG->dbhost    = 'moodledb';
 $CFG->dbname    = getenv('MOODLE_DOCKER_DBNAME');
 $CFG->dbuser    = getenv('MOODLE_DOCKER_DBUSER');
 $CFG->dbpass    = getenv('MOODLE_DOCKER_DBPASS');
@@ -48,7 +48,7 @@ $CFG->phpunit_dataroot  = '/var/www/phpunitdata';
 $CFG->phpunit_prefix = 't_';
 define('TEST_EXTERNAL_FILES_HTTP_URL', 'http://exttests:9000');
 
-$CFG->behat_wwwroot   = 'http://webserver';
+$CFG->behat_wwwroot   = 'http://moodleserver';
 $CFG->behat_dataroot  = '/var/www/behatdata';
 $CFG->behat_prefix = 'b_';
 $CFG->behat_profiles = array(
@@ -60,40 +60,5 @@ $CFG->behat_profiles = array(
 $CFG->behat_faildump_path = '/var/www/behatfaildumps';
 
 define('PHPUNIT_LONGTEST', true);
-
-if (getenv('MOODLE_DOCKER_APP')) {
-    $appport = getenv('MOODLE_DOCKER_APP_PORT') ?: 8100;
-
-    $CFG->behat_ionic_wwwroot = "http://moodleapp:$appport";
-}
-
-if (getenv('MOODLE_DOCKER_PHPUNIT_EXTRAS')) {
-    define('TEST_SEARCH_SOLR_HOSTNAME', 'solr');
-    define('TEST_SEARCH_SOLR_INDEXNAME', 'test');
-    define('TEST_SEARCH_SOLR_PORT', 8983);
-
-    define('TEST_SESSION_REDIS_HOST', 'redis');
-    define('TEST_CACHESTORE_REDIS_TESTSERVERS', 'redis');
-
-    define('TEST_CACHESTORE_MONGODB_TESTSERVER', 'mongodb://mongo:27017');
-
-    define('TEST_CACHESTORE_MEMCACHED_TESTSERVERS', "memcached0:11211\nmemcached1:11211");
-    define('TEST_CACHESTORE_MEMCACHE_TESTSERVERS', "memcached0:11211\nmemcached1:11211");
-
-    define('TEST_LDAPLIB_HOST_URL', 'ldap://ldap');
-    define('TEST_LDAPLIB_BIND_DN', 'cn=admin,dc=openstack,dc=org');
-    define('TEST_LDAPLIB_BIND_PW', 'password');
-    define('TEST_LDAPLIB_DOMAIN', 'ou=Users,dc=openstack,dc=org');
-
-    define('TEST_AUTH_LDAP_HOST_URL', 'ldap://ldap');
-    define('TEST_AUTH_LDAP_BIND_DN', 'cn=admin,dc=openstack,dc=org');
-    define('TEST_AUTH_LDAP_BIND_PW', 'password');
-    define('TEST_AUTH_LDAP_DOMAIN', 'ou=Users,dc=openstack,dc=org');
-
-    define('TEST_ENROL_LDAP_HOST_URL', 'ldap://ldap');
-    define('TEST_ENROL_LDAP_BIND_DN', 'cn=admin,dc=openstack,dc=org');
-    define('TEST_ENROL_LDAP_BIND_PW', 'password');
-    define('TEST_ENROL_LDAP_DOMAIN', 'ou=Users,dc=openstack,dc=org');
-}
 
 require_once(__DIR__ . '/lib/setup.php');
